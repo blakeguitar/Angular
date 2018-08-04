cc929614d0cf9325db3b55eae874042f256fabaa
Blake C Martin <blakeguitar@users.noreply.github.com>
2018-08-03 19:43:49 -0500

    HTTP B

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,20 +1,28 @@
+// Imports
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
-
 
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
 import { Hero } from './hero';
-//import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
+// Constants
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+//Injection
 @Injectable({
   providedIn: 'root'
 })
+
+/////////////////////////////////////////////////
+// Hero Service Class
 export class HeroService {
 
+  // Constructor
   constructor(
     private http: HttpClient,
     private messageService: MessageService) { }
@@ -49,26 +57,53 @@ export class HeroService {
     this.messageService.add(String(this.countMessages) + ` HeroService: ${message}`);
   }
 
+  /** PUT: update the hero on the server */
+  updateHero (hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
 
-/**
- * Handle Http operation that failed.
- * Let the app continue.
- * @param operation - name of the operation that failed
- * @param result - optional value to return as the observable result
- */
-private handleError<T> (operation = 'operation', result?: T) {
-  return (error: any): Observable<T> => {
+  /** POST: add a new hero to the server */
+  addHero (hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+      tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
 
-    // TODO: send the error to remote logging infrastructure
-    console.error(error); // log to console instead
+  /** DELETE: delete the hero from the server */
+  deleteHero (hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
 
-    // TODO: better job of transforming error for user consumption
-    this.log(`${operation} failed: ${error.message}`);
+    return this.http.delete<Hero>(url, httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
 
-    // Let the app keep running by returning an empty result.
-    return of(result as T);
-  };
-}
+
+	/**
+	 * Handle Http operation that failed.
+	 * Let the app continue.
+	 * @param operation - name of the operation that failed
+	 * @param result - optional value to return as the observable result
+	 */
+	private handleError<T> (operation = 'operation', result?: T) {
+	  return (error: any): Observable<T> => {
+
+	    // TODO: send the error to remote logging infrastructure
+	    console.error(error); // log to console instead
+
+	    // TODO: better job of transforming error for user consumption
+	    this.log(`${operation} failed: ${error.message}`);
+
+	    // Let the app keep running by returning an empty result.
+	    return of(result as T);
+	  };
+	}
 
 
 }
